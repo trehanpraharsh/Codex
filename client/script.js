c25869919a8892609cefe6e64abf1a5a02d3b171
trehanpraharsh <trehanpraharsh@gmail.com>
2023-08-27 21:53:21 +0530

    FrontEnd and Server almost done

--- client/script.js
+++ client/script.js
@@ -17,3 +17,71 @@ function loader(element) {
     }
   }, 300);
 }
+
+function typeText(element, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
+}
+
+function generateUniqueID() {
+  const timeStamp = Date.now();
+  const randomNumber = Math.random();
+  const hexaDecimalString = randomNumber.toString(16);
+
+  return `id-${timeStamp}-${hexaDecimalString}`;
+}
+
+function chatStripe(isAi, value, uniqueID) {
+  return `
+      <div class="wrapper ${isAi && "ai"}">
+        <div class="chat">
+          <div class="profile">
+            <img
+              src="${isAi ? bot : user}"
+              alt="${isAi ? "bot" : "user"}"
+            />
+          </div>
+          <div class="message" id=${uniqueID}>
+            ${value}
+          </div>
+        </div>
+      </div>
+    `;
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  //* user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
+
+  form.reset();
+
+  //* bot's chatstripe
+  const uniqueID = generateUniqueID();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueID);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueID);
+
+  loader(messageDiv);
+};
+
+form.addEventListener("submit", handleSubmit);
+//* if we press enter key whose keyCode = 13
+form.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    handleSubmit(e);
+  }
+});
---
+++ server/server.js
@@ -0,0 +1,54 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+// import { Configuration, OpenAIApi } from "openai";
+import OpenAI from "openai";
+
+dotenv.config();
+
+// const configuration = new Configuration({
+//   apiKey: process.env.OPEN_API_KEY,
+// });
+
+// const myApiKey = `${process.env.OPENAI_API_KEY}`;
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "Hello from CodeX",
+  });
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    const response = await openai.completions.create({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+
+    res.status(200).send({
+      bot: response.choices[0].text,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error });
+  }
+});
+
+app.listen(6969, () =>
+  console.log("Server is running on port http://localhost:6969")
+);
